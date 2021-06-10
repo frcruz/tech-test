ba77b235ff0bf70d038686984c5d44bc21e973ac
Francis Cruz <frcruz@ucsc.edu>
2021-06-10 14:09:20 -0700

    initial commit, project at end of interview

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,49 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <article>
+    <div>
+      <img src="images/alan-rails.jpeg" alt="Alan Rails">
+        <h5>Alan Rails</h5>
+    </div>
+    <div>
+      <div>
+        <div>STATUS</div>
+        <div>Dead</div>
+      </div>
+      <div>
+        <div>SPECIES</div>
+        <div>Human, Superhuman (Ghost trains summoner)</div>
+      </div>
+      <div>
+        <div>GENDER</div>
+        <div>Male</div>
+      </div>
+      <div>
+        <div>ORIGIN</div>
+        <div>unknown</div>
+      </div>
+      <div>
+        <div>LAST LOCATION</div>
+        <div>Worldender's lair</div>
+      </div>
+    </div>
+  </article>
+  <md-card>
+      <md-card-media-cover md-solid>
+        <md-card-media md-ratio="4:3">
+          <img src="/../assets/alan-rails.jpeg" alt="Alan Rails">
+        </md-card-media>
+
+        <md-card-area>
+          <md-card-header>
+            <span class="md-title">Solid background</span>
+            <span class="md-subhead">4/3 image</span>
+          </md-card-header>
+        </md-card-area>
+      </md-card-media-cover>
+    </md-card>
+
   </div>
 </template>
 
